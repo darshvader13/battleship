f19d48d35ae45937cd777c09204ca2792be2df36
darshvader13 <39998776+darshvader13@users.noreply.github.com>
2020-01-30 09:53:28 -0600

    Add files via upload

--- BattleShip.java
+++ BattleShip.java
@@ -1,29 +1,30 @@
 public class BattleShip
 {
-   public Battleship(String[][] a, int u)
+   public Battleship(String[][] a, String[][] v, int u)
    {
       private String[][] arr = a;
+      private String[][] visible = v;
       private int user = u;
    }
 
-   public static void SetVisibleShip1(String[][] a, int x, int y, String change)
+   public static void setVisibleShip1(int x, int y, String change)
    {
-      arr[x][y] = change;
+      visible[x][y] = change;
    }
    
-   public static void SetVisibleShip2(String[][] a, int x, int y, String change)
+   public static void setVisibleShip2(int x, int y, String change)
    {
-      arr[x][y] = change;
+      visible[x][y] = change;
    }
    
    public static String getVisibleShip1(int x, int y)
    {
-      return arr[x][y];
+      return visible[x][y];
    }
 
    public static String getVisibleShip2(int x, int y)
    {
-      return arr[x][y];
+      return visible[x][y];
    }
    
    public static String getBattleShip1(int x, int y)
