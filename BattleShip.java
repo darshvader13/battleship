9c44da5caea1fc0667d0604d4f02ca50676a4a21
darshvader13 <39998776+darshvader13@users.noreply.github.com>
2020-01-31 10:12:17 -0600

    Add files via upload
    if you use this it wont work because new argument is added darsh will figure it out(add battleship array to object creation)

--- BattleShip.java
+++ BattleShip.java
@@ -1,11 +1,15 @@
 public class BattleShip
 {
    private static String[][] visible = new String[10][10];
-   public BattleShip(String[][] v)
+   private static String[][] battleship = new String[10][10];
+   public BattleShip(String[][] v, String[][] b)
    {
       for (int i = 0; i < 10; i++)
          for (int j = 0; j < 10; j++)
             visible[i][j] = v[i][j];
+      for (int i = 0; i < 10; i++)
+         for (int j = 0; j < 10; j++)
+            battleship[i][j] = b[i][j];
    }
 
    public static void setVisibleShip1(int x, int y, String change)
@@ -26,6 +30,25 @@ public class BattleShip
    public static String getVisibleShip2(int x, int y)
    {
       return visible[x][y];
+   }
+   public static void setBattleShip1(int x, int y, String change)
+   {
+      battleship[x][y] = change;
+   }
+   
+   public static void setBattleShip2(int x, int y, String change)
+   {
+      battleship[x][y] = change;
+   }
+   
+   public static String getBattleShip1(int x, int y)
+   {
+      return battleship[x][y];
+   }
+
+   public static String getBattleShip2(int x, int y)
+   {
+      return battleship[x][y];
    }
 }   
   
