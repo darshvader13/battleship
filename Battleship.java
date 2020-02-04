7a9d839d71af017916925b748fcde35149fb529d
shuklaom <31251518+shuklaom@users.noreply.github.com>
2020-02-04 09:50:11 -0600

    Add files via upload

--- Battleship.java
+++ Battleship.java
@@ -1,28 +1,34 @@
 import java.util.*;
 public class Battleship{
 	
-	private static int[][] player1Map = {{1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 2, 2, 2, 2, 2, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 3, 0}, {0, 0, 5, 0, 0, 0, 0, 0, 3, 0}, {0, 0, 5, 0, 3, 0, 0, 0, 3, 0}, {0, 0, 0, 0, 3, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 3, 0, 0, 0, 0, 0}, {0, 5, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 5, 0, 0, 0, 0, 4, 4, 4, 0}};
+	private static int[][] player1Map = {{1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 2, 2, 2, 2, 2, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 8, 0}, {0, 0, 5, 0, 0, 0, 0, 0, 8, 0}, {0, 0, 5, 0, 3, 0, 0, 0, 8, 0}, {0, 0, 0, 0, 3, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 3, 0, 0, 0, 0, 0}, {0, 9, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 9, 0, 0, 0, 0, 4, 4, 4, 0}};
 		
-	private static int[][] player2Map = {{3, 3, 3, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 5, 0, 4, 4, 4, 0}, {0, 0, 0, 0, 5, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 5, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 5, 0, 0, 3}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 3}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 3}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {2, 2, 2, 2, 2, 0, 0, 0, 0, 0}};
+	private static int[][] player2Map = {{3, 3, 3, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 5, 0, 4, 4, 4, 0}, {0, 0, 0, 0, 5, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 5, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 5, 0, 0, 8}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 8}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 8}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {2, 2, 2, 2, 2, 0, 0, 0, 0, 0}};
 		
 	private static int[][] player1View = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 		
 	private static int[][] player2View = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 		
-	private static String[] x_cords = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	private static String[] y_cords = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 	
-	private static String x_cord_string = "    A  B  C  D  E  F  G  H  I  J";
+	private static String y_cord_string = "    A  B  C  D  E  F  G  H  I  J";
+	
+	
 	
 	public static void main(String a[]) throws Exception{
 		/**
 		 * 1 = Battleship(B)
 		 * 2 = Carrier(C)
 		 * 3 = Cruser(R)
+		 * 8 = Cruser(r)
 		 * 4 = Sub(S)
 		 * 5 = Destroyer(D)
+		 * 9 = Destroyer(d)
 		 * 6 = Empty Slot(View)
 		 * 7 = Ship Hit(view)
 		 */
+		
+		//Printing the board for both player 1 and 2.
 		/**String temp = "";
 		
 		System.out.println("Player 1 Map\n\n\n\n");
@@ -45,6 +51,10 @@ public class Battleship{
 					temp = " S ";
 				else if(player1Map[row][column] == 5)
 					temp = " D ";
+				else if(player1Map[row][column] == 8)
+					temp = " r ";
+				else if(player1Map[row][column] == 9)
+					temp = " d ";
 				else
 					temp = "ERROR";
 				System.out.print(temp);
@@ -74,15 +84,20 @@ public class Battleship{
 					temp2 = " S ";
 				else if(player2Map[row][column] == 5)
 					temp2 = " D ";
+				else if(player2Map[row][column] == 8)
+					temp = " r ";
+				else if(player2Map[row][column] == 9)
+					temp = " d ";
 				else
 					temp2 = "ERROR";
 				System.out.print(temp2);
             }
             System.out.println();
         }*/
-		
+		//Prints the views for player 1 and 2.
 		printViews();
 		
+		//input class and index variables for the 2D array.
 		Scanner sc = new Scanner(System.in);
 		boolean gameOn = true;
 		int x_index_1 = 0;
@@ -92,48 +107,135 @@ public class Battleship{
 		
 		while(gameOn){
 			try{
+				if ((checkGame1() == true) || (checkGame2() == true))
+				{
+					gameOn = false;
+					sc.close();
+					break;
+				}
 				//Player 1
 				System.out.println("Player 1: \n\n");
 				System.out.print("Enter the X-coordinate: ");
-				String x_cord_1 = sc.next();
-				if(x_cord_string.contains(x_cord_1.toUpperCase())){
-					System.out.print("Enter the Y-Coordinate: ");
-					int y_cord_1 = sc.nextInt();
-					y_index_2 = y_cord_1;
-					for(int x = 0; x < x_cords.length; x++){
-						if(x_cords[x].equalsIgnoreCase(x_cord_string.toUpperCase())){
-							x_index_1 = x;
+				String y_cord_1 = sc.next();
+				try{
+					if(y_cord_1.isEmpty()){
+						y_cord_1 = sc.next();
+					}
+					if(y_cord_string.contains(y_cord_1.toUpperCase()) && !y_cord_1.isEmpty()){
+						System.out.print("Enter the Y-Coordinate: ");
+						int x_cord_1 = sc.nextInt();
+						x_index_1 = (x_cord_1 - 1);
+						for(int y = 0; y < y_cords.length; y++){
+							if(y_cords[y].equalsIgnoreCase(y_cord_1.toUpperCase())){
+								y_index_1 = y;
+							}
 						}
 					}
+					else if(y_cord_1.equalsIgnoreCase("quit")){
+						System.out.println("Player 1 Quit The Game!");
+						System.exit(0);
+					}
+					else{
+						System.out.println("Wrong cord, Player 1. \n\n\tTurn Skipped!");
+						continue;
+					}					
+				} catch(Exception e){
+					System.out.println("An Error Has Occured!");
+					continue;
+				}
+				
+				
+				//Player 2
+				System.out.println("Player 2: \n\n");
+				System.out.print("Enter the X-coordinate: ");
+				String y_cord_2 = sc.next();
+				try{
+					if(y_cord_string.contains(y_cord_2.toUpperCase())){
+						System.out.print("Enter the Y-Coordinate: ");
+						int x_cord_2 = sc.nextInt();
+						x_index_2 = (x_cord_2 - 1);
+						for(int y = 0; y < y_cords.length; y++){
+							if(y_cords[y].equalsIgnoreCase(y_cord_2.toUpperCase())){
+								y_index_2 = y;
+							}
+						}
+					}					
+					else if(y_cord_2.equalsIgnoreCase("quit")){
+						System.out.println("Player 2 Quit The Game!");
+						System.exit(0);
+					}
+					else{
+						System.out.println("Wrong cord, Player 2. \n\n\tTurn Skipped!");
+						continue;
+					}
+				} catch(Exception e){
+					System.out.println("An Error Has Occured!");
+					continue;
+				}
+				
+				
+				//Checks for Hit or Miss, Player 1.
+				if(player2Map[x_index_1][y_index_1] != 0){
+					System.out.println("Player 1: \n\tShip Hit! \n\n\tAt Cords: (" + y_cords[y_index_1] + ", " + (x_index_1 + 1) + ")");
+					int s = player2Map[x_index_1][y_index_1];
+					player1View[x_index_1][y_index_1] = 7;
+					player2Map[x_index_1][y_index_1] = 7;
+					if ((checkSunk(s, player2Map)) != -1)
+						System.out.println("Ship Sunk!!");
+						System.out.println();
+					
+					
+				}
+				else if(player2Map[x_index_1][y_index_1] == 0){
+					System.out.println("Player 1: \n\tMiss. \n\n\tAt Cords: (" + y_cords[y_index_1] + ", " + (x_index_1 + 1) + ")");
+					player1View[x_index_1][y_index_1] = 6;
+					
 				}
 				else{
-					System.out.println("Wrong cord");
+					System.out.println("Check Skipped!");
 				}
 				
-				if(player2Map[x_index_1][y_index_1] != 0){
-					System.out.println("Ship Hit!!");
-					player1View[x_index_1][y_index_1] = 7;
-					printViews();
+				
+				//Checks for Hit or Miss, Player 2.
+				if(player1Map[x_index_2][y_index_2] != 0){
+					System.out.println("Player 2: \n\tShip Hit! \n\n\tAt Cords: (" + y_cords[y_index_2] + ", " + (x_index_2 + 1) + ")");
+					int s = player1Map[x_index_1][y_index_1];
+					player2View[x_index_2][y_index_2] = 7;
+					player1Map[x_index_2][y_index_2] = 7;
+					if ((checkSunk(s, player1Map)) != -1)
+						System.out.println("Ship Sunk!!");
+						System.out.println();
+					
+				}
+				else if(player1Map[x_index_2][y_index_2] == 0){
+					System.out.println("Player 2: \n\tMiss. \n\n\tAt Cords: (" + y_cords[y_index_2] + ", " + (x_index_2 + 1) + ")");
+					player2View[x_index_2][y_index_2] = 6;
+					
+				}
+				else{
+					System.out.println("Check Skipped!");
 				}
 				
-				break;
+				System.out.println("\n\n\n\n");
+				printViews();
+				System.out.println("\n\n\n");
 			} catch(Exception e){
-				System.out.println("An Error has occured.");
+				System.out.println("An Error has occured.\n\n\n" + e);
 			}
 		}
 		
 		
 	}
 	
-	
+	//Prints views for both players 1 and 2
 	public static void printViews(){
 		String temp3 = "";
 		System.out.println("\n\nPlayer 1 View\n\n\n\n");
-		System.out.println(x_cord_string);
+		System.out.println(y_cord_string);
 		for (int row = 0; row < 10; row++) {
 			System.out.print((row + 1) + " ");
 			if(row != 9){
-					System.out.print(" ");
+					System.out.print(" "); 
 				}
 			for (int column = 0; column < 10; column++) {
 				if(player1View[row][column] == 0)
@@ -155,9 +257,12 @@ public class Battleship{
 				else
 					temp3 = "ERROR";
 				System.out.print(temp3);
-            }
+		
+			}
             System.out.println();
+			
         }
+		
 		
 		String temp4 = "";
 		
@@ -182,14 +287,67 @@ public class Battleship{
 				else if(player2View[row][column] == 5)
 					temp4 = " D ";
 				else if(player2View[row][column] == 6)
-					temp3 = " O ";
+					temp4 = " O ";
 				else if(player2View[row][column] == 7)
-					temp3 = " X ";
+					temp4 = " X ";
 				else
 					temp4 = "ERROR";
 				System.out.print(temp4);
             }
             System.out.println();
+		
         }
 	}
+	
+	public static int checkSunk(int s, int[][]a)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				if (a[i][j] == s)
+				{
+					return -1;
+				}
+			}
+		}
+		return s;
+		
+	}
+	public static boolean checkGame1()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				if ((player1Map[i][j] != 0)||(player1Map[i][j] != 7))
+				{
+					return false;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Player 2 wins");
+		return true;
+		
+	}
+	public static boolean checkGame2()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				if ((player2Map[i][j] != 0)||(player2Map[i][j] != 7))
+				{
+					return false;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Player 1 wins");
+		return true;
+		
+	}
+	
+	
 }
